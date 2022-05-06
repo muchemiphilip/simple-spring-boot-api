1a02ffffec1e006573304449920370e35ea877b3
shemuchemi <shemuchemi@gmail.com>
2022-05-06 08:48:27 +0300

    Initial commit

---
+++ src/main/java/com/shemuchemi/simplespringbootapi/SimpleSpringBootApiApplication.java
@@ -0,0 +1,13 @@
+package com.shemuchemi.simplespringbootapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleSpringBootApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleSpringBootApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/shemuchemi/simplespringbootapi/SimpleSpringBootApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shemuchemi.simplespringbootapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleSpringBootApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
